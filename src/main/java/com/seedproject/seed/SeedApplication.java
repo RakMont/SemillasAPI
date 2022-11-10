7fddacc55c7f9b548c0cde8bcd2a8a10fa35bebc
Raquel Montero <raquel.montero@ucb.edu.bo>
2022-11-10 16:24:44 -0400

    run decript

--- src/main/java/com/seedproject/seed/SeedApplication.java
+++ src/main/java/com/seedproject/seed/SeedApplication.java
@@ -1,13 +1,26 @@
 package com.seedproject.seed;
 
+import com.seedproject.seed.services.EncripttionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.inject.Inject;
 
 @SpringBootApplication
-public class SeedApplication {
+public class SeedApplication implements CommandLineRunner {
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeedApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("encrypted " + this.bCryptPasswordEncoder.encode("8815992"));
+	}
 }
