9b24699102539703b5d7fcc47b79fd8770f5df2d
RakMont <nabogaria@gmail.com>
2023-09-12 14:41:32 -0400

    config

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,10 @@ COPY --from=build /target/seed-0.0.1-SNAPSHOT.jar seed.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-XX:MaxRAM=70m", "-jar", "seed.jar"]
 
+
+#FROM ibm-semeru-runtimes:open-11-jre-focal
+#EXPOSE 8080
+#COPY --from=build /target/seed-0.0.1-SNAPSHOT.jar seed.jar
+#ENV _JAVA_OPTIONS="-XX:MaxRAM=70m"
+#CMD java $_JAVA_OPTIONS -Djava.security.egd=file:/dev/./urandon -jar seed.jar
+#ENTRYPOINT ["java","-XX:MaxRAM=70m", "-jar", "seed.jar"]
--- src/main/java/com/seedproject/seed/SeedApplication.java
+++ src/main/java/com/seedproject/seed/SeedApplication.java
@@ -6,10 +6,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
 
 @SpringBootApplication
+@RestController
 public class SeedApplication implements CommandLineRunner {
 
 	@Autowired
@@ -27,5 +30,9 @@ public class SeedApplication implements CommandLineRunner {
 		System.out.println("encrypted " + this.bCryptPasswordEncoder.encode("denis"));
 
 		System.out.println("ids" +  this.encripttionService.encrypt("3"));
+	}
+	@GetMapping(path = {"/init"})
+	public String init(){
+		return "Inicializado";
 	}
 }
